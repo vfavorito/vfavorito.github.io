0bbcfa9565242752f9a310e112a557f38aacd88a
vfavorito <vince.favorito@gmail.com>
2021-04-27 20:17:14 -0500

    added bullet points to tech card

--- portfolio/src/components/Technology Card/techCard.jsx
+++ portfolio/src/components/Technology Card/techCard.jsx
@@ -9,7 +9,7 @@ function TechCard() {
             
                 <h4 className="cardHeader">Technology Proficiencies</h4>
                 {technologies.map(tech =>
-                    <p className="technology" key={tech}>{tech}</p>
+                    <p className="technology" key={tech}>• {tech}</p>
                 )}
             
         </div>
